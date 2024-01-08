644d014fa838b2b02193f02b40e91f61a6892a8b
tsannie <tsannie@student.42.fr>
2024-01-08 17:50:33 +0100

    [fix] img

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -3,17 +3,23 @@ const projects_list = [
     name: "Leaffliction",
     description:
       "Here, I perform the analysis, augmentation and transformation of a leaf dataset. After image classification using a Convolutional Neural Network.",
-    image:
-      "https://camo.githubusercontent.com/4a40452bf14786ccfb502f5524574fb593536bff9d06bd3c0a5c14d232646131/68747470733a2f2f692e696d6775722e636f6d2f59734d697936422e676966",
-    link: "https://github.com/arlaine4/Leaffliction",
+    image: "https://i.imgur.com/YsMiy6B.gif",
+    link: "https://github.com/tsannie/Leaffliction",
+    year: "2023",
+  },
+  {
+    name: "flappy_bird_NEAT",
+    description:
+      "This project aims to create a flappy bird game using pygame and the NEAT algorithm.",
+    image: "https://i.imgur.com/yvwAGCi.gif",
+    link: "https://github.com/tsannie/flappybird_NEAT",
     year: "2023",
   },
   {
     name: "multilayer_perceptron",
     description:
       "Create a neural network from scratch in python using numpy. My project is inspired by Keras.",
-    image:
-      "https://camo.githubusercontent.com/0aaa8227707588c1710914dfd71a06397998fbce15e61861068b4580b84c373c/68747470733a2f2f692e696d6775722e636f6d2f313843434731792e706e67",
+    image: "https://i.imgur.com/18CCG1y.png",
     link: "https://github.com/tsannie/multilayer_perceptron",
     year: "2023",
   },
@@ -21,8 +27,7 @@ const projects_list = [
     name: "dslr",
     description:
       "Data Science x Logistic Regression, is a project that aims to analyze a data set and predict Hogwarts House.",
-    image:
-      "https://thumbs.gfycat.com/DependableNastyBedbug-size_restricted.gif",
+    image: "https://i.imgur.com/70PtVql.gif",
     link: "https://github.com/tsannie/dslr",
     year: "2023",
   },
@@ -38,8 +43,7 @@ const projects_list = [
   {
     name: "linear_regression",
     description: "A simple linear regression algorithm in python.",
-    image:
-      "https://camo.githubusercontent.com/f4c962edcbc091280d0b9b4da2b0866776865cef327863a8775a7f93910a58ca/68747470733a2f2f692e696d6775722e636f6d2f614d64736c54702e676966",
+    image: "https://i.imgur.com/aMdslTp.gif",
     link: "https://github.com/tsannie/linear_regression",
     year: "2023",
   },
@@ -54,16 +58,14 @@ const projects_list = [
   {
     name: "minishell",
     description: "Shell implementation in C like bash.",
-    image:
-      "https://camo.githubusercontent.com/33c4415745a5b7f1eaa9fd3a9ee236654144fac734bb675a3eca54a89c996c3c/68747470733a2f2f692e696d6775722e636f6d2f736d6b427834342e676966",
+    image: "https://i.imgur.com/smkBx44.gif",
     link: "https://github.com/tsannie/minishell",
     year: "2021",
   },
   {
     name: "cube3d",
     description: "A simple raycasting engine in C like Wolfenstein 3D.",
-    image:
-      "https://camo.githubusercontent.com/b07adbe2a9fd707382f498ffb27147ed298aa9918fd24fa5c29c9cad24db8912/68747470733a2f2f692e696d6775722e636f6d2f494734726c68332e676966",
+    image: "https://i.imgur.com/IG4rlh3.gif",
     link: "https://github.com/tsannie/cub3d",
     year: "2021",
   },
@@ -87,8 +89,7 @@ const projects_list = [
     name: "push_swap",
     description:
       "Sort data on a stack, with a limited set of instructions, using the lowest possible number of actions.",
-    image:
-      "https://camo.githubusercontent.com/c270621dc9f17928a02ec3ac35c419458bb42fa4eec7df416971085fe9f99835/68747470733a2f2f692e696d6775722e636f6d2f4f5377386d66582e676966",
+    image: "https://i.imgur.com/OSw8mfX.gif",
     link: "https://github.com/tsannie/push_swap",
     year: "2021",
   },
